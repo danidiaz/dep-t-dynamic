bd927aff83ff83f64f62dcd5761dce455da0048a
Daniel Díaz <diaz_carrete@yahoo.com>
2021-11-14 15:22:38 +0100

    first typereps

--- lib/Control/Monad/Dep/Dynamic.hs
+++ lib/Control/Monad/Dep/Dynamic.hs
@@ -40,5 +40,18 @@ import Data.String
 import Data.Type.Equality (type (==))
 import Data.Dynamic
 import Data.Typeable
+import Type.Reflection qualified as R
+import Data.HashMap.Strict (HashMap)
+import Data.HashMap.Strict qualified as H
 
-data DynamicEnv = DynamicEnv
+data DynamicEnv (h :: Type -> Type) (m :: Type -> Type) = 
+    DynamicEnv (R.TypeRep h) (R.TypeRep m) (HashMap TypeRep Dynamic)
+
+emptyDynEnv :: forall h m . (Typeable h, Typeable m) => DynamicEnv h m 
+emptyDynEnv = DynamicEnv (R.typeRep @h) (R.typeRep @m) H.empty
+
+
+
+-- data InductiveEnv (rs :: [(Type -> Type) -> Type]) (h :: Type -> Type) (m :: Type -> Type) where
+--     AddDep :: forall r_ m rs h . h (r_ m) -> InductiveEnv rs h m -> InductiveEnv (r_ : rs) h m
+--     EmptyEnv :: forall m h . InductiveEnv '[] h m
