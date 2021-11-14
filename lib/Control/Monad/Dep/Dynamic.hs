8d8eba2c1710cfdcaca873684810d6cd3ddc3bb3
Daniel Díaz <diaz_carrete@yahoo.com>
2021-11-14 23:53:31 +0100

    SomeComponentRep

--- lib/Control/Monad/Dep/Dynamic.hs
+++ lib/Control/Monad/Dep/Dynamic.hs
@@ -17,6 +17,9 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 -- | This module
 module Control.Monad.Dep.Dynamic
@@ -46,6 +49,7 @@ import GHC.Generics qualified as G
 import GHC.Records
 import GHC.TypeLits
 import Type.Reflection qualified as R
+import Data.Hashable
 
 data DynamicEnv (h :: Type -> Type) (m :: Type -> Type)
   = DynamicEnv (HashMap TypeRep Dynamic)
@@ -78,7 +82,36 @@ data DepNotFound = DepNotFound TypeRep deriving (Show)
 instance Exception DepNotFound
 
 instance Phased DynamicEnv where
-    traverseH trans env = undefined
+    traverseH 
+        :: forall (h :: Type -> Type) (f :: Type -> Type) (g :: Type -> Type) (m :: Type -> Type). 
+        ( Applicative f 
+        , Typeable f
+        , Typeable g
+        , Typeable h
+        , Typeable m
+        )
+        => (forall x . h x -> f (g x)) 
+        -> DynamicEnv h m 
+        -> f (DynamicEnv g m)
+    traverseH trans (DynamicEnv dict) = DynamicEnv <$> H.traverseWithKey dynTrans dict
+      where
+      dynTrans k v = case k of
+        R.SomeTypeRep tr -> undefined
+
+    --    => (forall x . h x -> f (g x)) -> env_ h m -> f (env_ g m)
+
     liftA2H trans env env' = undefined
 
+data SomeComponentRep where
+    SomeComponentRep :: forall (a :: (Type -> Type) -> Type) . !(R.TypeRep a) -> SomeComponentRep
+
+instance Eq SomeComponentRep where
+    SomeComponentRep r1 == SomeComponentRep r2 = 
+        case r1 `R.eqTypeRep` r2 of
+          Just _  -> True
+          Nothing -> False
+
+instance Hashable SomeComponentRep where
+    hashWithSalt salt (SomeComponentRep tr) = hashWithSalt salt tr
+    hash (SomeComponentRep tr) = hash tr 
 
