733de4f4fdcc0478741968e6d5ed15fe403d5eb4
Daniel Díaz <diaz_carrete@yahoo.com>
2021-11-27 10:57:18 +0100

    unfoldphases

--- lib/Dep/SimpleChecked.hs
+++ lib/Dep/SimpleChecked.hs
@@ -1,3 +1,19 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 module Dep.SimpleChecked where
 
+import Data.Kind
+import Dep.Env
+import Data.Functor.Compose
+import GHC.TypeLits
+
 data CheckedEnv = CheckedEnv 
+
+type UnfoldPhases :: ((Type -> Type) -> (Type -> Type) -> Type) -> (Type -> Type) -> [Type -> Type] -> Type -> Type
+type family UnfoldPhases e_ m phases where
+    UnfoldPhases e_ m '[] = Constructor e_ m
+    UnfoldPhases e_ m (p ': ps) = p `Compose` UnfoldPhases e_ m ps
+
+
