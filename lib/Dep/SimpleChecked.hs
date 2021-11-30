ce745875668ef7ddd51bd176f56b4f7919643013
Daniel Díaz <diaz_carrete@yahoo.com>
2021-11-30 20:05:24 +0100

    constraints

--- lib/Dep/SimpleChecked.hs
+++ lib/Dep/SimpleChecked.hs
@@ -3,8 +3,13 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
 module Dep.SimpleChecked (CheckedEnv) where
 
+import Data.Typeable
 import Data.Kind
 import Dep.Env
 import Data.Functor.Compose
@@ -12,14 +17,33 @@ import GHC.TypeLits
 import Dep.Dynamic
 import Data.SOP qualified as SOP
 
-data CheckedEnv phases m = CheckedEnv (DynamicEnv (UnderConstruction phases DynamicEnv m) m)
+data CheckedEnv phases m = CheckedEnv (DynamicEnv (UnderConstruction phases m) m)
 
-type UnderConstruction :: [Type -> Type] -> ((Type -> Type) -> (Type -> Type) -> Type) -> (Type -> Type) -> Type -> Type
-newtype UnderConstruction phases e_ m x = UnderConstruction (ExpandPhases phases e_ m x)
+type UnderConstruction :: [Type -> Type] -> (Type -> Type) -> Type -> Type
+newtype UnderConstruction phases m x = UnderConstruction (ExpandPhases phases DynamicEnv m x)
 
 type ExpandPhases :: [Type -> Type] -> ((Type -> Type) -> (Type -> Type) -> Type) -> (Type -> Type) -> Type -> Type
 type family ExpandPhases phases e_ m where
     ExpandPhases '[] e_ m = Constructor e_ m
     ExpandPhases (p ': ps) e_ m  = p `Compose` ExpandPhases ps e_ m
+
+type HasAll :: [(Type -> Type) -> Type] -> (Type -> Type) -> Type -> Constraint
+type family HasAll rs m e where
+    HasAll '[] m e = ()
+    HasAll (r_ : rs) m e = (Has r_ m e, HasAll rs m e)
+
+type AllTypeable :: [k] -> Constraint
+type family AllTypeable xs where
+    AllTypeable '[] = ()
+    AllTypeable  (x : xs) = (Typeable x, AllTypeable xs)
+
+type MonadSatisfiesAll :: [(Type -> Type) -> Constraint] -> (Type -> Type) -> Constraint
+type family MonadSatisfiesAll cs m where
+    MonadSatisfiesAll '[] m = ()
+    MonadSatisfiesAll (c : cs) m = (c m, MonadSatisfiesAll cs m)
+
+checkedDep :: forall rs mcs phases r_ m . UnderConstruction phases m (r_ m) -> CheckedEnv phases m -> CheckedEnv phases m
+checkedDep = undefined
+
 
 
