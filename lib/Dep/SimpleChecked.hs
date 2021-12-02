5ab7e4823af75a4cdac4472bb8a923713a0cc121
Daniel Díaz <diaz_carrete@yahoo.com>
2021-12-02 20:54:43 +0100

    clean up

--- lib/Dep/SimpleChecked.hs
+++ lib/Dep/SimpleChecked.hs
@@ -1,49 +1,60 @@
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Dep.SimpleChecked (CheckedEnv) where
 
-import Data.Typeable
-import Data.Kind
-import Dep.Env
 import Data.Functor.Compose
-import GHC.TypeLits
-import Dep.Dynamic
+import Data.Kind
 import Data.SOP qualified as SOP
+import Data.Typeable
+import Dep.Dynamic
+import Dep.Env
+import GHC.TypeLits
 
-data CheckedEnv phases m = CheckedEnv (DynamicEnv (UnderConstruction phases (DynamicEnv Identity m)) m)
-
-type UnderConstruction :: [Type -> Type] -> Type -> Type -> Type
-newtype UnderConstruction phases e x = UnderConstruction (ExpandPhases phases e x)
-
-type ExpandPhases :: [Type -> Type] -> Type -> Type -> Type
-type family ExpandPhases phases e where
-    ExpandPhases '[] e = Constructor e
-    ExpandPhases (p ': ps) e = p `Compose` ExpandPhases ps e
+data CheckedEnv phases m = CheckedEnv (DynamicEnv (phases `Compose` Constructor (DynamicEnv Identity m)) m)
 
 type HasAll :: [(Type -> Type) -> Type] -> (Type -> Type) -> Type -> Constraint
 type family HasAll rs m e where
-    HasAll '[] m e = ()
-    HasAll (r_ : rs) m e = (Has r_ m e, HasAll rs m e)
-
-type AllTypeable :: [k] -> Constraint
-type family AllTypeable xs where
-    AllTypeable '[] = ()
-    AllTypeable  (x : xs) = (Typeable x, AllTypeable xs)
+  HasAll '[] m e = ()
+  HasAll (r_ : rs) m e = (Has r_ m e, HasAll rs m e)
 
 type MonadSatisfiesAll :: [(Type -> Type) -> Constraint] -> (Type -> Type) -> Constraint
 type family MonadSatisfiesAll cs m where
-    MonadSatisfiesAll '[] m = ()
-    MonadSatisfiesAll (c : cs) m = (c m, MonadSatisfiesAll cs m)
+  MonadSatisfiesAll '[] m = ()
+  MonadSatisfiesAll (c : cs) m = (c m, MonadSatisfiesAll cs m)
 
--- checkedDep :: forall rs mcs phases r_ m . (forall e_. UnderConstruction phases (e_ m) m (r_ m)) -> CheckedEnv phases m -> CheckedEnv phases m
--- checkedDep = undefined
+checkedDep ::
+  forall rs mcs r_ m phases.
+  ( SOP.All Typeable rs,
+    SOP.All Typeable mcs,
+    Typeable r_,
+    Typeable m,
+    Typeable phases,
+    HasAll rs m (DynamicEnv Identity m),
+    MonadSatisfiesAll mcs m
+  ) =>
+  -- | stuff
+  ( forall e n.
+    ( HasAll rs n e,
+      MonadSatisfiesAll mcs n
+    ) =>
+    (phases `Compose` Constructor e) (r_ n)
+  ) ->
+  -- | stuff
+  CheckedEnv phases m ->
+  CheckedEnv phases m
+checkedDep f (CheckedEnv de) = CheckedEnv (insertDep (f @(DynamicEnv Identity m) @m) de)
 
-
-
+-- depless/terminal dep (no constructor)
+-- phaselessDep (no phases, only the constructor) 
+--
